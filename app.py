9adf715456cc9d5e3d9a74d701182fe82324bb4f
Luc Nguyen <nguyentanluc.2111999@gmail.com>
2020-11-20 16:31:47 +0700

    7

--- app.py
+++ app.py
@@ -8,36 +8,32 @@ import os
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
-
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods = ["GET", "POST"])
 def index():
     return render_template('index.html')
 
-
-@app.route('/upload', methods=["GET", "POST"])
+@app.route('/upload', methods = ["GET", "POST"])
 def upload():
     if request.method == "POST":
         file = request.files["file"]
         file.save(os.path.join("static/upload", file.filename))
-        return render_template('data.html', data=file.filename)
+        return render_template('data.html', data = file.filename)
     return render_template('uploadcsv.html')
 
-
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods = ['POST'])
 def predict():
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
     output = round(prediction[0], 2)
     result = ""
-    if(output == 1):
+    if(output==1):
         result = "Có"
-    if(output == 0):
+    if(output==0):
         result = "Không"
     return render_template('index.html', prediction_text='Có ly hôn hay không: {}'.format(result))
 
-
-@app.route('/result', methods=['GET', 'POST'])
+@app.route('/result', methods=['GET','POST'])
 def data():
     # Hiển thị bảng CSV chưa có cột dự đoán
     # if request.method == 'POST':
@@ -63,46 +59,20 @@ def data():
                 final_features = [np.array(row)]
                 prediction = model.predict(final_features)
                 output = round(prediction[0], 2)
-                if(output == 1):
+                if(output==1):
                     row.append('1')
                     data.append(row)
-                if(output == 0):
+                if(output==0):
                     row.append('0')
                     data.append(row)
-        data.insert(0, ['Atr1', 'Atr2', 'Atr3', 'Atr4', 'Atr5', 'Atr6', 'Atr7', 'Atr8', 'Atr9', 'Atr10',
-                        'Atr11', 'Atr12', 'Atr13', 'Atr14', 'Atr15', 'Atr16', 'Atr17', 'Atr18', 'Atr19', 'Atr20',
-                        'Atr21', 'Atr22', 'Atr23', 'Atr24', 'Atr25', 'Atr26', 'Atr27', 'Atr28', 'Atr29', 'Atr30',
-                        'Atr31', 'Atr32', 'Atr33', 'Atr34', 'Atr35', 'Atr36', 'Atr37', 'Atr38', 'Atr39', 'Atr40',
-                        'Atr41', 'Atr42', 'Atr43', 'Atr44', 'Atr45', 'Atr46', 'Atr47', 'Atr48', 'Atr49', 'Atr50',
-                        'Atr51', 'Atr52', 'Atr53', 'Atr54', 'Class'])
+        data.insert(0,['Atr1','Atr2','Atr3','Atr4','Atr5','Atr6','Atr7','Atr8','Atr9','Atr10',
+                        'Atr11','Atr12','Atr13','Atr14','Atr15','Atr16','Atr17','Atr18','Atr19','Atr20',
+                        'Atr21','Atr22','Atr23','Atr24','Atr25','Atr26','Atr27','Atr28','Atr29','Atr30',
+                        'Atr31','Atr32','Atr33','Atr34','Atr35','Atr36','Atr37','Atr38','Atr39','Atr40',
+                        'Atr41','Atr42','Atr43','Atr44','Atr45','Atr46','Atr47','Atr48','Atr49','Atr50',
+                        'Atr51','Atr52','Atr53','Atr54','Class'])
         data = pd.DataFrame(data)
         return render_template('result.html', prediction_text_csv=data.to_html(header=False))
-
-@app.route('/tranform_view', methods=['GET', 'POST'])
-def tranform_view():
-    f = request.files['data_file']
-
-    # dùng hàm read_csv để đọc file vừa nhận được
-    data = pd.read_csv(f, names=None, index_col=None)
-
-    # lấy những cột và dòng giá trị để dự đoán
-    dt = data.iloc[:, :]
-
-    # dự đoán kết quả
-    dubao = model.predict(dt)
-
-    # thêm cột label và giá trị đã dự đoán được vào file
-    dt["Label"] = dubao
-
-    # xuất ra file results.csv
-    dt.to_csv('./static/results.csv', header=True)
-
-    # mở lại file csv với tên results.csv
-    file = open("./static/results.csv", "r")
-
-    # trả về file results.csv cho người dùng
-    return Response(file, mimetype="text/csv", headers={"Content-disposition": "attachment; filename=results.csv"})
-
 
 if __name__ == "__main__":
     app.run(debug=True)
