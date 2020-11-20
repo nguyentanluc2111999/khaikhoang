6567c3037ccd35f8a3773678c076f1cb339d6000
Luc Nguyen <nguyentanluc.2111999@gmail.com>
2020-11-20 15:40:46 +0700

    1

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+import pandas as pd
+import pickle
+import csv
+import os
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/upload', methods = ["GET", "POST"])
+def upload():
+    if request.method == "POST":
+        file = request.files["file"]
+        file.save(os.path.join("static/upload", file.filename))
+        return render_template('data.html', data = file.filename)
+    return render_template('uploadcsv.html')
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    output = round(prediction[0], 2)
+    result = ""
+    if(output==1):
+        result = "Có"
+    if(output==0):
+        result = "Không"
+    return render_template('index.html', prediction_text='Có ly hôn hay không: {}'.format(result))
+
+@app.route('/result', methods=['GET','POST'])
+def data():
+    if request.method == 'POST':
+        f = request.form['csvfile']
+        path = "static/upload/" + f
+        data = []
+        with open(path) as file:
+            csvfile = csv.reader(file)
+            next(file)
+            for row in csvfile:
+                final_features = [np.array(row)]
+                prediction = model.predict(final_features)
+                output = round(prediction[0], 2)
+                if(output==1):
+                    row.append('1')
+                    data.append(row)
+                if(output==0):
+                    row.append('0')
+                    data.append(row)
+        data.insert(0,['Atr1','Atr2','Atr3','Atr4','Atr5','Atr6','Atr7','Atr8','Atr9','Atr10',
+                        'Atr11','Atr12','Atr13','Atr14','Atr15','Atr16','Atr17','Atr18','Atr19','Atr20',
+                        'Atr21','Atr22','Atr23','Atr24','Atr25','Atr26','Atr27','Atr28','Atr29','Atr30',
+                        'Atr31','Atr32','Atr33','Atr34','Atr35','Atr36','Atr37','Atr38','Atr39','Atr40',
+                        'Atr41','Atr42','Atr43','Atr44','Atr45','Atr46','Atr47','Atr48','Atr49','Atr50',
+                        'Atr51','Atr52','Atr53','Atr54','Class'])
+        data = pd.DataFrame(data)
+        return render_template('result.html', prediction_text_csv=data.to_html(header=False))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from sklearn import preprocessing
+from scipy.io import arff
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold
+import pickle
+
+# Load du lieu
+df = pd.read_csv('https://raw.githubusercontent.com/CanhHo1004/dataset/main/divorce/divorce.csv',';')
+# print(df)
+
+# Kiem tra gia tri null
+total =  df.isnull().sum()
+# print(total)
+
+X = df.drop('Class', axis=1)
+y = df['Class']
+
+def score_dt(X, y):
+  kf = KFold(n_splits = 10)
+  total = 0
+  for train_index, test_index in kf.split(X):
+    X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+    model = DecisionTreeClassifier()
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    total += accuracy_score(y_test, y_pred)
+  pickle.dump(model, open('model.pkl','wb'))
+  return total/10
+
+test = score_dt(X, y) # < 0.95 tai k-Fold = 15
+print(test)
